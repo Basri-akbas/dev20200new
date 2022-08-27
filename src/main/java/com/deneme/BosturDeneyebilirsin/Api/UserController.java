3f37e12218b23be03604a6af0701b170905b3ca7
Samet Öztürk <0zturksamet4901@gmail.com>
2022-08-27 19:06:31 +0200

    postgreSQL dependency eklendi

--- src/main/java/com/deneme/BosturDeneyebilirsin/Api/UserController.java
+++ src/main/java/com/deneme/BosturDeneyebilirsin/Api/UserController.java
@@ -1,6 +1,9 @@
 package com.deneme.BosturDeneyebilirsin.Api;
 
 import com.deneme.BosturDeneyebilirsin.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,24 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("user")
 public class UserController {
-
+    @Autowired
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
-
         this.userService = userService;
     }
 
-    @GetMapping(path = "/get")
-    public static void getir() {
+    /*
 
+
+    @GetMapping(path = "/get")
+
+    public static void getir() {
 
     }
 
     @GetMapping(path = "/post")
     public static void gonder() {
 
-    }
+    }*/
 
     // get, post, put, delete yazilacak ve calisir vaziyette olacak
     // UnitTest yazilacak => JUnit 5 kullanilacak
--- src/main/java/com/deneme/BosturDeneyebilirsin/service/UserService.java
+++ src/main/java/com/deneme/BosturDeneyebilirsin/service/UserService.java
@@ -1,16 +1,16 @@
 package com.deneme.BosturDeneyebilirsin.service;
 
 import com.deneme.BosturDeneyebilirsin.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
-@RequestMapping("/user")
 public class UserService {
-
+    @Autowired
     private final UserRepository userRepository;
 
-
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
