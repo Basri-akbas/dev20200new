34dc80ba0b3bc5d30415ea2edae3b49782c8f684
Samet Öztürk <0zturksamet4901@gmail.com>
2022-08-27 21:07:32 +0200

    GET,PUT,POST,DELETE olusturuldu

--- src/main/java/com/deneme/BosturDeneyebilirsin/Api/UserController.java
+++ src/main/java/com/deneme/BosturDeneyebilirsin/Api/UserController.java
@@ -1,15 +1,19 @@
 package com.deneme.BosturDeneyebilirsin.Api;
 
+import com.deneme.BosturDeneyebilirsin.Entity.User;
 import com.deneme.BosturDeneyebilirsin.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("user")
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private final UserService userService;
@@ -19,21 +23,42 @@ public class UserController {
         this.userService = userService;
     }
 
-    /*
 
 
-    @GetMapping(path = "/get")
-
-    public static void getir() {
-
+    //add user
+   @PostMapping("/add")
+    public ResponseEntity<User> addNewUser(@RequestBody User user){
+        User newUser = userService.addNewUser(user);
+        return  new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "/post")
-    public static void gonder() {
 
-    }*/
+   //get All user
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> getAllUser = userService.getAllUser();
+        return new ResponseEntity<>(getAllUser,HttpStatus.OK);
+    }
 
-    // get, post, put, delete yazilacak ve calisir vaziyette olacak
-    // UnitTest yazilacak => JUnit 5 kullanilacak
-    // Mockito Framework bakilacak
+    //get user by id
+    @GetMapping("find/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable("id") Long id){
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    //update user
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        User updateUser = userService.updateUser(user);
+        return new ResponseEntity<>(updateUser,HttpStatus.OK);
+    }
+
+    //delete User by id
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable("id") Long id){
+        userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
